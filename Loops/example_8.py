32fc2013d732968b76ea8410feb693f783889339
Abdullah Fareed <abdullahfareed454@gmail.com>
2021-01-19 20:25:09 +0500

    loops example 8.

---
+++ Loops/example_8.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lowercase = 'abcdefghijklmnopqrstuvwxyz'
+for upper, lower in zip(uppercase, lowercase):
+    print(upper, lower)
+
