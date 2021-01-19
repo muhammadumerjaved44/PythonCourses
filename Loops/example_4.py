a96e6cd98c2aed801fa94577bcb4a5618f28ea45
Abdullah Fareed <abdullahfareed454@gmail.com>
2021-01-19 20:18:52 +0500

    loop example 4.

---
+++ Loops/example_4.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+stuff = ['hello', 'hi', 'how are you doing', 'im fine', 'how about you']
+length_of_stuff = len(stuff)
+index = 0
+while index < length_of_stuff:
+    print(stuff[index])
+index += 1
