5135bb3fbf700e9d08c93d38b3660721fb0a2715
Abdullah Fareed <abdullahfareed454@gmail.com>
2021-01-19 16:11:43 +0500

    manipulate string with methods 3.

---
+++ Strings/example_8.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+string1 = " Filet Mignon"
+string2 = "Brisket "
+string3 = " Cheeseburger "
+print(string1.strip()) # Could also use .lstrip()
+print(string2.strip()) # Could also use .rstrip()
+print(string3.strip())
