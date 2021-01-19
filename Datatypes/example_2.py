3f50024fed410552676a820fb7eaf5093f277cc3
Abdullah Fareed <abdullahfareed454@gmail.com>
2021-01-19 14:59:53 +0500

    convert data types int float complex.

---
+++ Datatypes/example_2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+x = 1 # int
+y = 2.8 # float
+z = 5+1j # complex
+#convert from int to float:
+a = float(x)
+3
+#convert from float to int:
+b = int(y)
+#convert from int to complex:
+c = complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
