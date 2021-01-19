e5ef6992a02faaeeb0cbedd0ac009c10e6369bb4
Abdullah Fareed <abdullahfareed454@gmail.com>
2021-01-19 19:54:40 +0500

    conditions example 2.

---
+++ Conditions/examples_2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# One line if else statement:
+a = 2
+b = 330
+print("A") if a > b else print("B")
+# One line if else statement, with 3 conditions
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
