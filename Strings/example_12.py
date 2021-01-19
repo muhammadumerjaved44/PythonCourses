0befc8ffcb04854243a056ad1e490ae197a8cd81
Abdullah Fareed <abdullahfareed454@gmail.com>
2021-01-19 16:19:12 +0500

    replace words with other words.

---
+++ Strings/example_12.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+phrase = "Somebody said something to Samantha."
+phrase = phrase.replace("s", "x")
+print(phrase)
+# NOTE: This leaves the capital "S" unchanged, so the
+# output will be "Somebody xaid xomething to Samantha."
