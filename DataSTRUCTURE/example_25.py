cc00a921b126fc9184879af8f8dcbc2bd9e27eec
Abdullah Fareed <abdullahfareed454@gmail.com>
2021-01-19 18:18:29 +0500

    sets exercise 3.

---
+++ DataSTRUCTURE/example_25.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# The union() method returns a new set with all items from both sets:
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+set3 = set1.union(set2)
+print(set3)
+# Return a set that contains the items that exist in both set x, and set y:
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.intersection(y)
+print(z)
+# Keep the items that are not present in both sets:
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.symmetric_difference_update(y)
+print(z)
