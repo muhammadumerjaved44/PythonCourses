f0c1b0f7aefecef91cb799d5a4f3368153fbb4f9
Abdullah Fareed <abdullahfareed454@gmail.com>
2021-01-19 20:21:07 +0500

    loops example 5.

---
+++ Loops/example_5.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+print(x)
+for x in range(2, 30, 3):
+    print(x)
+for x in range(6):
+    print(x)
+else:
+    print("Finally finished!")
+
