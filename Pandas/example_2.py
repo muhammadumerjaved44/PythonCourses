ca41bebe172c807dd5c3979879c3e594341685e0
Abdullah Fareed <abdullahfareed454@gmail.com>
2021-01-22 18:45:15 +0500

    pandas example-2.

---
+++ Pandas/example_2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+data_dicts = [
+{'name':"john","gender":'male','age':45},
+{'name':"mary", 'gender':"female",'age':19},
+{'name':"peter",'gender':'male', 'age':34}
+]
+
